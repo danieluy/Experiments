afaf5d3842a4a878ce999769cdc00fe1912d83c8
danieluy <danielsosa.dev@gmail.com>
2017-09-12 17:19:04 -0300

    Added path dependency

--- OAuth2/app.js
+++ OAuth2/app.js
@@ -2,15 +2,16 @@
 
 const express = require('express');
 const api = require('./routers/api');
+const path = require('path');
 
 const PORT = 3000
 
 const app = express();
 app.use('/api', api)
 app.get('/', (req, res) => {
-  res.status(200).sendfile('./public/index.html')
+  res.status(200).sendFile(path.join(__dirname, '/public/index.html'))
 })
 app.listen(PORT, () => {
   console.log('OAuth 2.0 Experiments App listening on:')
-  console.log(`http:\\\\localhost:${PORT}`)
+  console.log(`http://localhost:${PORT}`)
 })
